b50d1b1de9a4f5c55250782423a43bd8579824b8
vangeest <49232516+vangeest@users.noreply.github.com>
2020-06-02 09:56:57 +0200

    Eerste poging
    startscherm door GEE, niet gekeken of het werkt

--- script.js
+++ script.js
@@ -17,10 +17,10 @@
 /* globale variabelen die je gebruikt in je game */
 /* ********************************************* */
 
-const UITLEG = 0;
+const STARTSCHERM = 0;
 const SPELEN = 1;
 const GAMEOVER = 2;
-var spelStatus = SPELEN;
+var spelStatus = STARTSCHERM;
 
 var spelerX = 200; // x-positie van speler
 var spelerY = 100; // y-positie van speler
@@ -41,6 +41,12 @@ var score = 0; // aantal behaalde punten
 /*      functies die je gebruikt in je game      */
 /* ********************************************* */
 
+/**
+ * Tekent het startscherm
+ */
+var tekenStartscherm = function () {
+  tekst("startscherm",100,100);
+};
 
 /**
  * Tekent het speelveld
@@ -161,6 +167,12 @@ function setup() {
  */
 function draw() {
   switch (spelStatus) {
+    case STARTSCHERM:
+        tekenStartscherm();
+        if (keyIsDown(32)) {// 32=spatie
+           spelStatus = SPELEN;
+        }
+        break;
     case SPELEN:
       beweegVijand();
       beweegKogel();
